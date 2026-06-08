4bfc8e559856c21ccfd729ce58caad3c84ff5247
Dmitry Kuzmin <dmitrykuzmin00@gmail.com>
2026-06-08 12:00:54 +0300

    reexport test-rng from ark-std

--- ferveo-tdec/src/lib.rs
+++ ferveo-tdec/src/lib.rs
@@ -37,8 +37,9 @@ pub use key_share::{
 pub mod bls12_381;
 
 /// Re-exports [rand::Rng] crate.
-pub mod rand_traits {
+pub mod rand_utils {
     pub use rand::Rng;
+    pub use ark_std::test_rng;
 }
 
 #[derive(Debug, thiserror::Error)]
